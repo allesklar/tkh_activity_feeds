1ea29942054f46cf0092baf9de396c94e812dc61
Swami Atma <swami@ohlalaweb.com>
2014-08-23 15:02:45 +0200

    The index view has friendly dates from the tkh_toolbox gem.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,7 @@ class Activity < ActiveRecord::Base
 
   belongs_to :doer, touch: true
 
-  scope :chronologically, -> { order('id') }
-  scope :by_recent, -> { order('id desc') }
+  scope :chronologically, -> { order('created_at') }
+  scope :by_recent, -> { order('created_at desc') }
 
 end
