0d523d79bed41b7ce0e17335d0304ab31f8b3a27
Swami Atma <swami@TenThousandHours.eu>
2015-08-09 09:31:10 +0200

    Added scope for yesterday's activity.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,8 @@ class Activity < ActiveRecord::Base
 
   belongs_to :doer, touch: true
 
+  # tkh_mailing_list sends daily digest with yesterday's activity
+  scope :yesterdays, -> { where('created_at >= ? AND created_at <= ?', 1.day.ago.beginning_of_day, 1.day.ago.end_of_day ) }
   scope :chronologically, -> { order('created_at') }
   scope :by_recent, -> { order('created_at desc') }
 
