a2cb7ae90e529a49e272cd00b8aaf2d64d4246cf
Swami Atma <swami@TenThousandHours.eu>
2015-09-09 12:52:10 +0200

    Admin can remove activity items from index view.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -8,4 +8,10 @@ class ActivitiesController < ApplicationController
     switch_to_admin_layout
   end
 
+  def destroy
+    activity = Activity.find(params[:id])
+    activity.destroy
+    redirect_to activities_path, notice: "<span class='glyphicon glyphicon-heart'></span> <strong>Success</strong> This activity item has been deleted.".html_safe
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    get "activities" => "activities#index", :as => "activities"
+    # get "activities" => "activities#index", :as => "activities"
+    resources :activities
   end
 end
